2310fa7981ce1a201aeecb08dcb192757ec93037
gustavo rocha <assessoriagustavorocha@outlook.com>
2023-01-20 01:12:24 -0300

    vs01

---
+++ lacosCondicionais.java
@@ -0,0 +1,86 @@
+//package JavaGeneration;
+
+//public class Repeticao {
+
+	//public static void main(String[] args) {
+
+//    int numero = 0;
+ //   int contador = 1;
+
+  //  System.out.println("\n Digite um número (1-10):");
+   // leia(numero);
+
+  //  do {
+    //  System.out.printf("\n", numero, "x", contador, "=", numero * contador);
+     // contador++;
+  //  }
+   // while (contador <= 10);
+ // }
+
+//	private static void leia(int numero) {
+		// TODO Auto-generated method stub
+		
+//	}
+//}
+
+//package JavaGeneration;
+
+//public class Ex02Repeticao {
+
+//	public static void main(String[] args) {
+	//    int firstNumber;
+	  //  int secoundNumber;
+
+	    //for(int x = 0; x < 4; x++){
+	      //int eCrescente = 1;
+	     // System.out.printf("\nDigite o primeiro número do intervalo: ");
+	     // leia(firstNumber);
+	     // System.out.printf("\nDigite o segundo número do intervalo: ");
+	     // leia(secoundNumber);
+
+	   //   if(firstNumber < secoundNumber e firstNumber % 3 == 0)
+	     //   System.out.printf("\n",firstNumber, " é múltiplo de 5 e 3");
+	     // else if(firstNumber < secoundNumber e firstNumber % 3 != 0)
+	       // System.out.printf("\n",firstNumber, " não é múltiplo de 5 e 3");
+	     // else
+	      //  System.out.printf("Intervalo inválido!");
+
+	   //   if(firstNumber < secoundNumber e secoundNumber % 3 == 0)
+	     //   System.out.printf("\n", secoundNumber, " é múltiplo de 5 e 3");
+	      
+	     // else if(firstNumber < secoundNumber e secoundNumber % 3 != 0)
+	       // System.out.printf("\n", secoundNumber, " não é múltiplo de 5 e 3");
+//	    }
+
+//	  }
+//	}
+//package JavaGeneration;
+
+//public class Ex04Repeticao {
+
+	//public static void main(String[] args) {
+		//int continua = 1;
+			//    int idade = 0;
+			  //  int somaTotalMenor21 = 0;
+			    //int somaTotalMaior50 = 0;
+
+//			    if(continua == 1){
+//			     System.out.printf("Digite uma idade: ");
+//			      leia(idade);
+
+//			      if(idade < 21 && idade >0)
+//			        somaTotalMenor21++;
+			      
+//			      else if(idade > 50)
+//			        somaTotalMaior50++;
+
+//			      if(idade < 0)
+//			        continua = 0;
+//			    }
+
+	//		    System.out.printf("\nTotal de pessoas menores de 21 anos: ", somaTotalMenor21);
+		//	    System.out.printf("\nTotal de pessoas maiores de 50 anos: ", somaTotalMaior50);
+
+
+//	}
+//}
