5e04df19b50268bf3c5e4705989b5eadea72122c
gustavo rocha <assessoriagustavorocha@outlook.com>
2023-01-26 16:30:49 -0300

    versão02

--- bloco 2 - banco de dados/blogpessoal/src/main/java/org/generation/blogpessal/controller/PostagemController.java
+++ bloco 2 - banco de dados/blogpessoal/src/main/java/org/generation/blogpessal/controller/PostagemController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.generation.blogpessal.model.Postagem;
 import org.generation.blogpessal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +26,10 @@ public class PostagemController {
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id){
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 }
